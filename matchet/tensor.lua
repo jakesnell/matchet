7d768d9b35709cc8e5e3cf9edc7b8e85b9dc2e96
Jake Snell <jsnell@cs.toronto.edu>
2016-07-27 20:46:01 -0400

    add uniquecount

--- matchet/tensor.lua
+++ matchet/tensor.lua
@@ -9,19 +9,31 @@ matchet.unique = argcheck{
     doc='contiguous tensor for which to find unique values',
     check=function(x) return x:isContiguous() end},
    call = function(x)
-      local vals = tds.Hash()
-      local xdata = torch.data(x)
-      for i=0,x:nElement()-1 do
-         vals[tonumber(xdata[i])] = true
-      end
-
-      local ret = torch.Tensor(#vals):typeAs(x)
+      local counts = matchet.uniquecount(x)
+      local ret = torch.Tensor(#counts):typeAs(x)
       local i = 1
-      for k,v in pairs(vals) do
+      for k,v in pairs(counts) do
          ret[i] = k
          i = i + 1
       end
       ret = torch.sort(ret)
       return ret
+   end
+}
+
+matchet.uniquecount = argcheck{
+   {name='x',
+    type='torch.*Tensor',
+    doc='contiguous tensor for which to find unique values',
+    check=function(x) return x:isContiguous() end},
+   call = function(x)
+      local counts = tds.Hash()
+      local xdata = torch.data(x)
+      local ind
+      for i=0,x:nElement()-1 do
+         ind = tonumber(xdata[i])
+         counts[ind] = (counts[ind] or 0) + 1
+      end
+      return counts
    end
 }
--- test/tensor.lua
+++ test/tensor.lua
@@ -6,3 +6,18 @@ function tests.unique()
 
    tester:eq(matchet.unique(x), torch.LongTensor({1, 2, 3, 5, 9}))
 end
+
+function tests.uniquecount()
+   local x = torch.LongTensor({{1, 1, 2, 2},
+                               {2, 5, 5, 7},
+                               {9, 4, 4, 4}})
+   local v = matchet.uniquecount(x)
+
+   tester:eq(#v, 6)
+   tester:eq(v[1], 2)
+   tester:eq(v[2], 3)
+   tester:eq(v[4], 3)
+   tester:eq(v[5], 2)
+   tester:eq(v[7], 1)
+   tester:eq(v[9], 1)
+end
